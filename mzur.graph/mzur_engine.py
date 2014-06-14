43091d2c10f217e44e8850d53ee13ced6b374643
Ari Ramdial <ari.ramdial@gmail.com>
2014-06-14 01:07:57 -0400

    Update mzur_engine.py

--- mzur.graph/mzur_engine.py
+++ mzur.graph/mzur_engine.py
@@ -13,14 +13,17 @@ g.add_proxy("location", Location)
 g.add_proxy("budget", Budget)
 
 def CreateUser(p_name, p_age, p_gender, p_email, p_sportList):
-	person = g.user.create(email=p_email)
-	person.name = p_name
-	person.age = p_age 
-	person.gender = p_gender
-	person.sportChosen = p_sportList
-	person.save()
+	# Create a new person node and pass in the properties
+	if g.user.index.lookup(email=p_email)==null:
+		person = g.user.create(email=p_email)
+		person.name = p_name
+		person.age = p_age 
+		person.gender = p_gender
+		person.sportChosen = p_sportList
+		person.save()
 
 def CreateSport(p_name, p_experience_level, p_fitness_level):
+	#Create a new sport and pass in the properties
 	if g.sport.index.lookup(name=p_name)==null:
 		newSport = g.sport.create(p_name)
 		newSport.experience_level = p_experience_level
@@ -28,16 +31,21 @@ def CreateSport(p_name, p_experience_level, p_fitness_level):
 		newSport.save()
 	
 def ChoseSport(self,p_email,p_sport):
+	#Look up the user
 	person = g.user.index.lookup(email=p_email)
+	#Look up the sport they chose
 	sportSelected = g.sport.index.lookup(name=p_sport)
+	#Add that sport to the list of all chosen sports
 	person.sportChosen.append(p_sport)
+	#Update the user node with the new sport list
 	g.user.update(person, person.sport)
+	#Create an edge between person and the sport they chose
 	g.choses.create(person,sportSelected)	
 
  
-def AssignRoutine(self, email, sportChosen):
-	self.email = email
-	self.sportChosen = sportChosen
-	userID = g.user.index.lookup(name=self.email)
-	sports = g.user.index.lookup(name=self.sportChosen)
+def AssignRoutine(p_email, p_sportList):
+	# Look up the person
+	person = g.user.index.lookup(email=p_email)
+	# Look up the sports they chose
+	sports = g.sport.index.lookup(name=p_sportList)
 	print "User chose sport"
