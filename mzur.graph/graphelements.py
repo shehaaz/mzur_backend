120c6b24480e486e55ca8ab6829bde5bd6f2018b
Ari Ramdial <ari.ramdial@gmail.com>
2014-06-13 23:34:26 -0400

    Update graphelements.py

--- mzur.graph/graphelements.py
+++ mzur.graph/graphelements.py
@@ -5,14 +5,15 @@ from bulbs.property import String, Integer, DateTime
 from bulbs.utils import current_datetime
 
 class User(Node):
+	
 	element_type = "user"
 
-    name = String(nullable=False)
-    age = Integer()
-    gender = String(nullable=False)
-    email = String(nullable=False)
-    sportChosen = List()
-    isAvailable = List()
+	name = String(nullable=False)
+	age = Integer()
+	gender = String(nullable=False)
+	email = String(nullable=False)
+	sportChosen = List()
+	isAvailable = List()
 
 class Sport(Node):
 	element_type = "sport"
