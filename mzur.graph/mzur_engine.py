bdee397689b902315d0f05cbc4b13cff220884b2
Ari Ramdial <ari.ramdial@gmail.com>
2014-06-14 00:47:10 -0400

    Update mzur_engine.py

--- mzur.graph/mzur_engine.py
+++ mzur.graph/mzur_engine.py
@@ -12,13 +12,20 @@ g.add_proxy("sport", Sport)
 g.add_proxy("location", Location)
 g.add_proxy("budget", Budget)
 
-def CreateUser(self, p_name, p_age, p_gender, p_email, p_sportList):
+def CreateUser(p_name, p_age, p_gender, p_email, p_sportList):
 	person = g.user.create(email=p_email)
 	person.name = p_name
 	person.age = p_age 
 	person.gender = p_gender
 	person.sportChosen = p_sportList
 	person.save()
+
+def CreateSport(p_name, p_experience_level, p_fitness_level):
+	if g.sport.index.lookup(name=p_name)==null:
+		newSport = g.sport.create(p_name)
+		newSport.experience_level = p_experience_level
+		newSport.fitness_level = p_fitness_level
+		newSport.save()
 	
 def ChoseSport(self,p_email,p_sportList):
 	person = g.user.index.lookup(email=p_email)
